2f03c2eb5fa0792b3974fed21d01b5d4c19a23f2
Rebecca Anne Sealfon <ras2198@columbia.edu>
2012-03-25 01:13:36 -0400

    New Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SealfonTermProject::Application.config.secret_token = 'c362cb0968db52f0da252dd19bb63950d1855df815de00789e27f8091bbfe4fdee709d0fa40a0abce25e296505dbc10a5398d3b0a4e97142f42ab20b55bb822f'
